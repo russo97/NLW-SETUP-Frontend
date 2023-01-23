e3d734b4b99798f3668bb89deb73b6b9c92dfd8a
Wanderson Silva <wandersonsilva768@gmail.com>
2023-01-22 23:34:24 -0300

    feat: adicionado o componente SummaryTable, onde serão listados os hábitos retornados da API

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,23 @@
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+export function SummaryTable () {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-2">
+        {
+          weekDays.map((weekDay, i) => {
+            return (
+              <div
+                key={`${weekDay}-${i}`}
+                className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
+              >
+                {weekDay}
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
